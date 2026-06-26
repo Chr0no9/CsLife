4b506ffa4d86d03d9536d64768e32ca9a02ba1d0
Marco Nasirpour <mnasirpo@ucsc.edu>
2026-06-25 22:05:16 -0700

    v3

--- Character.js
+++ Character.js
@@ -1,3 +1,21 @@
+export function createPlayer(name, traitName) {
+    const selectedTrait = traits.find(t => t.name === traitName);
+
+    player.name = name;
+    player.trait = selectedTrait.name;
+
+    player.health = selectedTrait.stats.health;
+    player.happiness = selectedTrait.stats.happiness;
+    player.money = selectedTrait.stats.money;
+    player.intellect = selectedTrait.stats.intellect;
+    player.coding = selectedTrait.stats.coding;
+    player.social = selectedTrait.stats.social;
+
+    console.log(player);
+}
+
+
+
 const player = {
     name: "",
     trait: "",
@@ -9,52 +27,56 @@ const player = {
     intellect: 0,
     coding: 0,
     social: 0,
-    
+
     gpa: 3.0,
-    relationshipStatus: "single",
+    relationshipStatus: "Single",
     internship: null,
     jobOffer: null
 };
 
-const trait = {
-    name: "Studious",
-
-    bonuses: {
-        health: +30,
-        happiness: +40,
-        money: +250,
-        intellect: +90,
-        coding: +70,
-        social: +5
+const traits = [
+    {
+        name: "Studious",
+        stats: {
+            health: 30,
+            happiness: 40,
+            money: 250,
+            intellect: 90,
+            coding: 70,
+            social: 5
+        }
+    },
+    {
+        name: "Well Rounded",
+        stats: {
+            health: 50,
+            happiness: 50,
+            money: 500,
+            intellect: 50,
+            coding: 50,
+            social: 50
+        }
+    },
+    {
+        name: "Social Butterfly",
+        stats: {
+            health: 70,
+            happiness: 95,
+            money: 0,
+            intellect: 40,
+            coding: 10,
+            social: 90
+        }
+    },
+    {
+        name: "Wealthy",
+        stats: {
+            health: 80,
+            happiness: 80,
+            money: 10000,
+            intellect: 30,
+            coding: 10,
+            social: 30
+        }
     }
-    name: "Well_Rounded",
-
-    bonuses: {
-        health: +50,
-        happiness: +50,
-        money: +500,
-        intellect: +50,
-        coding: +50,
-        social: +50
-    }
-    name: "Social_Butterfly",
-
-    bonuses: {
-        health: +70,
-        happiness: +95,
-        money: +250,
-        intellect: +40,
-        coding: +10,
-        social: +90
-    }
-    name: "Wealthy",
-
-    bonuses: {
-        health: +80,
-        happiness: +80,
-        money: +10000,
-        intellect: +30,
-        coding: +10,
-        social: +30
-    }
-};
+];
